e44285b9b543c868f1e71f7d928ea56f30071fd9
Etiene Francis Okokon <67736726+Tienzy1@users.noreply.github.com>
2022-05-30 00:48:07 +0100

    Update hard_skills.py

--- Solution/hard_skills.py
+++ Solution/hard_skills.py
@@ -1,5 +1,7 @@
+# Open the files in read mode
 with open(r"C:\Users\ADMIN\Desktop\ML_internship_dataset\Raw_Skills_Dataset.csv", 'r',encoding="utf8") as file1:
     with open(r"C:\Users\ADMIN\Desktop\ML_internship_dataset\Example_Technical_Skills.csv", 'r', encoding="utf8") as file2:
+# The set intersection() method returns a set that contains the similarity between file1 and file2       
         same = set(file1).intersection(file2)
 with open('Hard_Skills_Dataset.csv', 'w') as file_out:
     for line in same:
