0a6d82e0fad32e331b50242e47410a0e54d97aba
Etiene Francis Okokon <67736726+Tienzy1@users.noreply.github.com>
2022-05-29 23:53:52 +0100

    Delete hard_skills.py

--- hard_skills.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\ADMIN\Desktop\ML_internship_dataset\Raw_Skills_Dataset.csv", 'r',encoding="utf8") as file1:
-    with open(r"C:\Users\ADMIN\Desktop\ML_internship_dataset\Example_Technical_Skills.csv", 'r', encoding="utf8") as file2:
-        same = set(file1).intersection(file2)
-with open('Hard_Skills_Dataset.csv', 'w') as file_out:
-    for line in same:
-        file_out.write(line)
